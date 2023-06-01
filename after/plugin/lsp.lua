e2dcb7526bbd6f75bd9e83390f005d08a3b208ee
Quinten Leung <currybot50@gmail.com>
2023-05-31 18:41:44 -0700

    Updated configs!

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,6 +3,8 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.ensure_installed({
   'pylsp',
+  'golangci_lint_ls',
+  'eslint',
 })
 
 lsp.nvim_workspace()
--- lua/Config/packer.lua
+++ lua/Config/packer.lua
@@ -42,6 +42,9 @@ return require('packer').startup(function(use)
 	  }
   }
   use('theprimeagen/vim-be-good')
+  use("folke/zen-mode.nvim")
+  use("github/copilot.vim")
+  use("eandrju/cellular-automaton.nvim")
+  use("laytan/cloak.nvim")
 end)
-
 
