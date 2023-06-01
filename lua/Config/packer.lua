9920fe0c32bf264449d1fc71a7869e37864132ea
Quinten Leung <currybot50@gmail.com>
2023-06-01 00:50:37 -0700

    Added copilot and toggleterm

--- lua/Config/packer.lua
+++ lua/Config/packer.lua
@@ -46,5 +46,8 @@ return require('packer').startup(function(use)
   use("github/copilot.vim")
   use("eandrju/cellular-automaton.nvim")
   use("laytan/cloak.nvim")
+  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+  require("toggleterm").setup()
+end}
 end)
 
